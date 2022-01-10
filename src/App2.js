25f3569025feab83c6941a6f3de7e6fa39be2362
Sifei <sifei@users.noreply.github.com>
2022-01-10 04:40:09 -0500

    Add files via upload

---
+++ src/App2.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import React, {useState} from "react";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify, {Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig)
+
+function App() {
+  const [username,setUsername] = useState("")
+  const funcc = async () => {
+    let user = await Auth.currentAuthenticatedUser();
+    console.log(user)
+    const { attributes } = user;
+    const { username } = user;
+    setUsername(username)
+  }
+  funcc()
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <div>
+          <h3 id="userName"> Welcome {username} </h3>          
+        </div>      
+        <a
+          className="App-link"
+          href="https://tsui-wakeupsafe.research.chop.edu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wakeup Safe
+        </a>
+ 
+      </header>
+      
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
